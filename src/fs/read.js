72e0634b657c1ffde58bd7969e24ec9647c10d67
Eugene Lositsky <eugenelositsky@gmail.com>
2025-10-26 03:00:30 +0500

    fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,26 @@
+import { join } from 'node:path';
+import { stat, readFile } from 'node:fs/promises';
+
 const read = async () => {
-  // Write your code here
+  const fileExists = async (path) => {
+    return stat(path)
+      .then(stats => stats.isFile())
+      .catch(() => false);
+  };
+
+  const folder = 'files';
+  const path = join(folder, 'fileToRead.txt');
+
+  const isFileExists = await fileExists(path);
+
+  if (!isFileExists) {
+    throw new Error('FS operation failed');
+  }
+
+  const file = await readFile(path, 'utf-8').catch(() => {
+    throw new Error('FS operation failed');
+  });
+  console.log(file);
 };
 
 await read();
