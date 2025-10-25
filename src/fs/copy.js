44c32cd1add39768d1375198f7533aee220baf8f
Eugene Lositsky <eugenelositsky@gmail.com>
2025-10-26 01:32:57 +0500

    fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { join } from 'node:path';
+import { stat, cp } from 'node:fs/promises';
+
 const copy = async () => {
-  // Write your code here
+  const folderExists = async (path) => {
+    return stat(path)
+      .then(stats => stats.isDirectory())
+      .catch(() => false);
+  };
+  
+  const src = join('files');
+  const dest = join('files_copy');
+
+  const srcExists = await folderExists(src);
+  const destExists = await folderExists(dest);
+
+  if (!srcExists || destExists) {
+    throw new Error('FS operation failed');
+  }
+
+  await cp(src, dest, { recursive: true }).catch(() => {
+    throw new Error('FS operation failed');
+  });
 };
 
 await copy();
