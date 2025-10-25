8344b61cc6336a0168eaabecf425733a0a96ee00
Eugene Lositsky <eugenelositsky@gmail.com>
2025-10-26 01:51:33 +0500

    fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import { join } from 'node:path';
+import { stat, rename as fsRename } from 'node:fs/promises';
+
 const rename = async () => {
-  // Write your code here
+  const fileExists = async (path) => {
+    return stat(path)
+      .then(stats => stats.isFile())
+      .catch(() => false);
+  };
+
+  const folder = 'files';
+  const oldPath = join(folder, 'wrongFilename.txt');
+  const newPath = join(folder, 'properFilename.md');
+
+  const isOldFileExists = await fileExists(oldPath);
+  const isNewFileExists = await fileExists(newPath);
+
+  if (!isOldFileExists || isNewFileExists) {
+    throw new Error('FS operation failed');
+  }
+
+  await fsRename(oldPath, newPath).catch(() => {
+    throw new Error('FS operation failed');
+  });
 };
 
 await rename();
