428d06af8ccc54b8b5becb4e323d0b838b2b7660
Eugene Lositsky <eugenelositsky@gmail.com>
2025-10-26 02:53:21 +0500

    fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { join } from 'node:path';
+import { stat, readdir } from 'node:fs/promises';
+
 const list = async () => {
-  // Write your code here
+  const folderExists = async (path) => {
+    return stat(path)
+      .then(stats => stats.isDirectory())
+      .catch(() => false);
+  };
+
+  const folder = join('files');
+  
+  const isFolderExists = await folderExists(folder);
+
+  if (!isFolderExists) {
+    throw new Error('FS operation failed');
+  }
+
+  const files = await readdir(folder).catch(() => {
+    throw new Error('FS operation failed');
+  });
+  console.log(files);
 };
 
 await list();
